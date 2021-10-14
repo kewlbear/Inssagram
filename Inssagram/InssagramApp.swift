cfd3a2453a826a458c5e3ce9fcbe70894cb08d93
Changbeom Ahn <kewlbear@gmail.com>
2021-10-14 11:44:00 +0900

    Initial Commit

---
+++ Inssagram/InssagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InssagramApp.swift
+//  Inssagram
+//
+//  Created by 안창범 on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct InssagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
